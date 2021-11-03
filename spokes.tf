46037af0d542ae08788c41f0e88ada0f85439855
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2021-11-03 16:21:47 -0400

    Update spokes.tf

--- spokes.tf
+++ spokes.tf
@@ -1,156 +1,156 @@
 variable cidrs_spoke1 {
-  type = list(string)
-}
-variable cidrs_spoke2 {
-  type = list(string)
-}
-variable vm_image {
-  default = "ubuntu-os-cloud/ubuntu-1604-lts"
-}
-
-variable vm_type {
-  default = "f1-micro"
-}
-
-variable vm_user {}
-
-variable vm_scopes {
-  type = list(string)
-
-  default = [
-    "https://www.googleapis.com/auth/cloud.useraccounts.readonly",
-    "https://www.googleapis.com/auth/devstorage.read_only",
-    "https://www.googleapis.com/auth/logging.write",
-    "https://www.googleapis.com/auth/monitoring.write",
-  ]
-}
-
-
-# --------------------------------------------------------------------------------------------------------------------------
-# Creates 2 x Spoke VPC Networks and peer them to the trust/hub network.
-
-module "vpc_spoke1" {
-  source               = "./modules/vpc/"
-  vpc                  = "${local.prefix}-vpc-A"
-  delete_default_route = true
-  allowed_sources      = ["0.0.0.0/0"]
-
-  subnets = {
-    "vpc-A-${var.region}" = {
-      region = var.region,
-      cidr   = var.cidrs_spoke1[0]
-    }
+    type = list(string)
   }
-}
-
-resource "google_compute_network_peering" "spoke1_to_trust" {
-  name         = "${module.vpc_spoke1.vpc_name}-${module.vpc_trust.vpc_name}"
-  network      = module.vpc_spoke1.vpc_id
-  peer_network = module.vpc_trust.vpc_id
-  export_custom_routes = false
-  import_custom_routes = true 
-}
-
-resource "google_compute_network_peering" "trust_to_spoke1" {
-  name         = "${module.vpc_trust.vpc_name}-${module.vpc_spoke1.vpc_name}"
-  network      = module.vpc_trust.vpc_id
-  peer_network = module.vpc_spoke1.vpc_id
-  export_custom_routes = true
-  import_custom_routes = false 
-}
-
-
-module "vpc_spoke2" {
-  source               = "./modules/vpc/"
-  vpc                  = "${local.prefix}-vpc-B"
-  delete_default_route = true
-  allowed_sources      = ["0.0.0.0/0"]
-
-  subnets = {
-    "vpc-B-${var.region}" = {
-      region = var.region,
-      cidr   = var.cidrs_spoke2[0]
-    }
+  variable cidrs_spoke2 {
+    type = list(string)
   }
-}
-
-
-resource "google_compute_network_peering" "spoke2_to_trust" {
-  name         = "${module.vpc_spoke2.vpc_name}-${module.vpc_trust.vpc_name}"
-  network      = module.vpc_spoke2.vpc_id
-  peer_network = module.vpc_trust.vpc_id
-  export_custom_routes = false
-  import_custom_routes = true 
-}
-
-resource "google_compute_network_peering" "trust_to_spoke2" {
-  name         = "${module.vpc_trust.vpc_name}-${module.vpc_spoke2.vpc_name}"
-  network      = module.vpc_trust.vpc_id
-  peer_network = module.vpc_spoke2.vpc_id
-  export_custom_routes = true
-  import_custom_routes = false 
-}
-
-# --------------------------------------------------------------------------------------------------------------------------
-# Create a ubuntu GCE instance in each spoke network.
-
-resource "google_compute_instance" "spoke1" {
-  name                      = "${local.prefix}-vpc-A-vm1"
-  machine_type              = var.vm_type
-  zone                      = data.google_compute_zones.main.names[0]
-  can_ip_forward            = false
-  allow_stopping_for_update = true
-  metadata_startup_script   = file("${path.module}/bootstrap_files/ubuntu_credentials.sh")
-
-  metadata = {
-    serial-port-enable = true
-    ssh-keys           = fileexists(var.public_key_path) ? "${var.vm_user}:${file(var.public_key_path)}" : ""
-  }
-
-  network_interface {
-    subnetwork = module.vpc_spoke1.subnet_self_link["vpc-A-${var.region}"]
-    network_ip = cidrhost(var.cidrs_spoke1[0], 10)
-  }
-
-  boot_disk {
-    initialize_params {
-      image = var.vm_image
-    }
-  }
-
-  service_account {
-    scopes = var.vm_scopes
+  variable vm_image {
+    default = "ubuntu-os-cloud/ubuntu-1604-lts"
   }
   
-}
-
-
-resource "google_compute_instance" "spoke2" {
-  name                      = "${local.prefix}-vpc-B-vm1"
-  machine_type              = var.vm_type
-  zone                      = data.google_compute_zones.main.names[0]
-  can_ip_forward            = false
-  allow_stopping_for_update = true
-  metadata_startup_script   = file("${path.module}/bootstrap_files/ubuntu_credentials.sh")
-
-  metadata = {
-    serial-port-enable = true
-    ssh-keys           = fileexists(var.public_key_path) ? "${var.vm_user}:${file(var.public_key_path)}" : ""
+  variable vm_type {
+    default = "f1-micro"
   }
-
-  network_interface {
-    subnetwork = module.vpc_spoke2.subnet_self_link["vpc-B-${var.region}"]
-    network_ip = cidrhost(var.cidrs_spoke2[0], 10)
+  
+  variable vm_user {}
+  
+  variable vm_scopes {
+    type = list(string)
+  
+    default = [
+      "https://www.googleapis.com/auth/cloud.useraccounts.readonly",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+    ]
   }
-
-  boot_disk {
-    initialize_params {
-      image = var.vm_image
+  
+  
+  # --------------------------------------------------------------------------------------------------------------------------
+  # Creates 2 x Spoke VPC Networks and peer them to the trust/hub network.
+  
+  module "vpc_spoke1" {
+    source               = "./modules/vpc/"
+    vpc                  = "${local.prefix}-vpc-a"
+    delete_default_route = true
+    allowed_sources      = ["0.0.0.0/0"]
+  
+    subnets = {
+      "vpc-a-${var.region}" = {
+        region = var.region,
+        cidr   = var.cidrs_spoke1[0]
+      }
     }
   }
-
-  service_account {
-    scopes = var.vm_scopes
+  
+  resource "google_compute_network_peering" "spoke1_to_trust" {
+    name         = "${module.vpc_spoke1.vpc_name}-${module.vpc_trust.vpc_name}"
+    network      = module.vpc_spoke1.vpc_id
+    peer_network = module.vpc_trust.vpc_id
+    export_custom_routes = false
+    import_custom_routes = true 
   }
-
-}
+  
+  resource "google_compute_network_peering" "trust_to_spoke1" {
+    name         = "${module.vpc_trust.vpc_name}-${module.vpc_spoke1.vpc_name}"
+    network      = module.vpc_trust.vpc_id
+    peer_network = module.vpc_spoke1.vpc_id
+    export_custom_routes = true
+    import_custom_routes = false 
+  }
+  
+  
+  module "vpc_spoke2" {
+    source               = "./modules/vpc/"
+    vpc                  = "${local.prefix}-vpc-b"
+    delete_default_route = true
+    allowed_sources      = ["0.0.0.0/0"]
+  
+    subnets = {
+      "vpc-b-${var.region}" = {
+        region = var.region,
+        cidr   = var.cidrs_spoke2[0]
+      }
+    }
+  }
+  
+  
+  resource "google_compute_network_peering" "spoke2_to_trust" {
+    name         = "${module.vpc_spoke2.vpc_name}-${module.vpc_trust.vpc_name}"
+    network      = module.vpc_spoke2.vpc_id
+    peer_network = module.vpc_trust.vpc_id
+    export_custom_routes = false
+    import_custom_routes = true 
+  }
+  
+  resource "google_compute_network_peering" "trust_to_spoke2" {
+    name         = "${module.vpc_trust.vpc_name}-${module.vpc_spoke2.vpc_name}"
+    network      = module.vpc_trust.vpc_id
+    peer_network = module.vpc_spoke2.vpc_id
+    export_custom_routes = true
+    import_custom_routes = false 
+  }
+  
+  # --------------------------------------------------------------------------------------------------------------------------
+  # Create a ubuntu GCE instance in each spoke network.
+  
+  resource "google_compute_instance" "spoke1" {
+    name                      = "${local.prefix}-vpc-a-vm1"
+    machine_type              = var.vm_type
+    zone                      = data.google_compute_zones.main.names[0]
+    can_ip_forward            = false
+    allow_stopping_for_update = true
+    metadata_startup_script   = file("${path.module}/bootstrap_files/ubuntu_credentials.sh")
+  
+    metadata = {
+      serial-port-enable = true
+      ssh-keys           = fileexists(var.public_key_path) ? "${var.vm_user}:${file(var.public_key_path)}" : ""
+    }
+  
+    network_interface {
+      subnetwork = module.vpc_spoke1.subnet_self_link["vpc-a-${var.region}"]
+      network_ip = cidrhost(var.cidrs_spoke1[0], 10)
+    }
+  
+    boot_disk {
+      initialize_params {
+        image = var.vm_image
+      }
+    }
+  
+    service_account {
+      scopes = var.vm_scopes
+    }
+    
+  }
+  
+  
+  resource "google_compute_instance" "spoke2" {
+    name                      = "${local.prefix}-vpc-b-vm1"
+    machine_type              = var.vm_type
+    zone                      = data.google_compute_zones.main.names[0]
+    can_ip_forward            = false
+    allow_stopping_for_update = true
+    metadata_startup_script   = file("${path.module}/bootstrap_files/ubuntu_credentials.sh")
+  
+    metadata = {
+      serial-port-enable = true
+      ssh-keys           = fileexists(var.public_key_path) ? "${var.vm_user}:${file(var.public_key_path)}" : ""
+    }
+  
+    network_interface {
+      subnetwork = module.vpc_spoke2.subnet_self_link["vpc-b-${var.region}"]
+      network_ip = cidrhost(var.cidrs_spoke2[0], 10)
+    }
+  
+    boot_disk {
+      initialize_params {
+        image = var.vm_image
+      }
+    }
+  
+    service_account {
+      scopes = var.vm_scopes
+    }
+  
+  }
